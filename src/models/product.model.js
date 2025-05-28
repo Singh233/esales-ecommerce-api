1f16b34427b18b33f4081564525f822d7b9908b6
Sanambir Singh <Sanambir123@gmail.com>
2025-05-28 23:56:40 +0530

    feat: add product model and plugins for soft delete and JSON transformation

---
+++ src/models/product.model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const { softDelete, toJSON } = require('./plugins/index.js');
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    colors: {
+      type: [String],
+      default: [],
+    },
+    sizes: {
+      type: [String],
+      default: [],
+    },
+    rating: {
+      rate: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0,
+      },
+      count: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+productSchema.plugin(softDelete);
+productSchema.plugin(toJSON);
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
